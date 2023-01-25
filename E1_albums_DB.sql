29c82f970b62abf5d680fd5c463b550ca1826a46
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-01-25 16:27:30 +0100

    Add files via upload

---
+++ E1_albums_DB.sql
@@ -0,0 +1,18 @@
+CREATE TABLE albums (
+	id INT,
+	artist_id VARCHAR(50),
+	album_title VARCHAR(50),
+	year_released INT
+
+	
+);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (1, 2, 'Are You Experienced', 1967);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (2, 2, 'Axis: Bold as Love', 1967);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (3, 1, 'Dirty Mind', 1980);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (4, 2, 'Electric Ladyland', 1968);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (5, 3, 'Abraxas', 1970);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (6, 1, '1999', 1982);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (7, 3, 'Santana III', 1971);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (8, 3, 'Santana', 1969);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (9, 1, 'Prince', 1979);
+INSERT INTO albums (id, artist_id, album_title, year_released) VALUES (10, 1, 'Controversy', 1981);
