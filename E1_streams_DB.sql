7380bb29061b17936492e32adfa12a0982d4d3b4
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-01-25 16:28:47 +0100

    Add files via upload

---
+++ E1_streams_DB.sql
@@ -0,0 +1,55 @@
+CREATE TABLE streams (
+	id INT,
+	artist_id INT,
+	album_id INT,
+	song_title VARCHAR (120),
+	date DATE,
+	number_of_streams INT,
+	pay_per_stream DECIMAL (10,4)
+	
+);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (1, 1, 9, 'I Wanna Be Your Lover', '2023-01-01', 597, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (2, 1, 6, 'Little Red Corvette', '2023-01-01', 497, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (3, 1, 6, 'D.M.S.R.', '2023-01-01', 217, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (4, 1, 3, 'Uptown', '2023-01-01', 9748, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (5, 1, 3, 'Do It All NIght', '2023-01-01', 208, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (6, 2, 8, 'Jingo', '2023-01-01', 418, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (7, 2, 7, 'Taboo', '2023-01-01', 108, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (8, 2, 5, 'Samba Pa Ti', '2023-01-01', 497, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (9, 2, 5, 'Incident at Neshabur', '2023-01-01', 57, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (10, 2, 7, 'Everything''s Coming Our Way', '2023-01-01', 612, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (11, 3, 1, 'The Wind Cries Mary', '2023-01-01', 248, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (12, 3, 4, 'Burning of the Midnight Lamp', '2023-01-01', 94, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (13, 3, 4, 'All Along the Watchtower', '2023-01-01', 674, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (14, 3, 2, 'Bold as Love', '2023-01-01', 1002, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (15, 3, 2, 'If 6 Was 9', '2023-01-01', 497, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (16, 1, 9, 'I Wanna Be Your Lover', '2023-01-02', 415, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (17, 1, 6, 'Little Red Corvette', '2023-01-02', 1572, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (18, 1, 6, 'D.M.S.R.', '2023-01-02', 408, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (19, 1, 3, 'Uptown', '2023-01-02', 697, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (20, 1, 3, 'Do It All NIght', '2023-01-02', 1847, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (21, 2, 8, 'Jingo', '2023-01-02', 947, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (22, 2, 7, 'Taboo', '2023-01-02', 514, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (23, 2, 5, 'Samba Pa Ti', '2023-01-02', 5479, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (24, 2, 5, 'Incident at Neshabur', '2023-01-02', 712, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (25, 2, 7, 'Everything''s Coming Our Way', '2023-01-02', 147, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (26, 3, 1, 'The Wind Cries Mary', '2023-01-02', 947, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (27, 3, 4, 'Burning of the Midnight Lamp', '2023-01-02', 641, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (28, 3, 4, 'All Along the Watchtower', '2023-01-02', 218, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (29, 3, 2, 'Bold as Love', '2023-01-02', 547, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (30, 3, 2, 'If 6 Was 9', '2023-01-02', 694, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (31, 1, 9, 'I Wanna Be Your Lover', '2023-01-03', 7485, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (32, 1, 6, 'Little Red Corvette', '2023-01-03', 5747, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (33, 1, 6, 'D.M.S.R.', '2023-01-03', 105, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (34, 1, 3, 'Uptown', '2023-01-03', 512, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (35, 1, 3, 'Do It All NIght', '2023-01-03', 808, 0.013);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (36, 2, 8, 'Jingo', '2023-01-03', 599, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (37, 2, 7, 'Taboo', '2023-01-03', 1639, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (38, 2, 5, 'Samba Pa Ti', '2023-01-03', 909, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (39, 2, 5, 'Incident at Neshabur', '2023-01-03', 68, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (40, 2, 7, 'Everything''s Coming Our Way', '2023-01-03', 647, 0.0033);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (41, 3, 1, 'The Wind Cries Mary', '2023-01-03', 9874, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (42, 3, 4, 'Burning of the Midnight Lamp', '2023-01-03', 1015, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (43, 3, 4, 'All Along the Watchtower', '2023-01-03', 1547, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (44, 3, 2, 'Bold as Love', '2023-01-03', 674, 0.0064);
+INSERT INTO streams (id, artist_id, album_id, song_title, date, number_of_streams, pay_per_stream) VALUES (45, 3, 2, 'If 6 Was 9', '2023-01-03', 2457, 0.0064);
