e9a03cf4d5fbaede0fba05fcf4235e5693b7fbd6
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-01-25 16:26:29 +0100

    Add files via upload

---
+++ E1_artist_DB.sql
@@ -0,0 +1,8 @@
+CREATE TABLE artist (
+	id INT,
+	artist_name VARCHAR(50)
+	
+);
+INSERT INTO artist (id, artist_name) VALUES (1, 'Prince');
+INSERT INTO artist (id, artist_name) VALUES (2, 'Jimi Hendrix');
+INSERT INTO artist (id, artist_name) VALUES (3, 'Santana');
